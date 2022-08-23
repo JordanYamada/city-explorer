8b5f7751307329485d5bde0db1deec44d472143e
Jordan Yamada <sadaotomokane@gmail.com>
2022-08-22 23:53:25 -0700

    adding the event handlers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    }
+  }
+
+  handleInput = (e) => {
+    let city = e.target.value;
+    this.setState({
+      city: city
+    });
+  };
+
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    let response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+    console.log(response.data[0]);
+    // save that data in state
+  }
+
+  render (){
+  return (
+    <Form onSubmit={this.handleSubmit}>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Choose a city</Form.Label>
+      <Form.Control aria-label="Default select example" onInput={this.handleInput}/>
+    </Form.Group>
+    <Button  variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
        );
+}
 }
 
 export default App;
