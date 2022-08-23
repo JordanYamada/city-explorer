1640420f976f1884c836dc7e2be55cff10c68b67
Jordan Yamada <sadaotomokane@gmail.com>
2022-08-23 14:44:14 -0700

    added header and main js files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import './App.css';
 
@@ -24,24 +25,52 @@ class App extends React.Component {
     });
   };
 
-  handleCitySubmit = async (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    let response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-    console.log(response.data[0]);
-    // save that data in state
-  }
+    try {
+      let response = await axios.get(
+        `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      );
+
+      this.setState({
+        cityData: response.data[0],
+        // map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${response.data[0].lat},${response.data[0].lon}&zoom=12`,
+      });
+    } catch (error) {
+      this.setState({
+        error: error.message,
+      });
+      console.log(error);
+    }
+  };
 
   render (){
   return (
+    <>
     <Form onSubmit={this.handleSubmit}>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Group className="mb-3" controlId="city">
       <Form.Label>Choose a city</Form.Label>
-      <Form.Control aria-label="Default select example" onInput={this.handleInput}/>
+      <Form.Control 
+      aria-label="Default select example"
+      type="text"
+      name="city"
+      onInput={this.handleInput}/>
     </Form.Group>
-    <Button  variant="primary" type="submit">
-      Submit
+    <Button  
+    variant="primary" 
+    type="submit">
+      Explore!
     </Button>
   </Form>
+   <Card>
+   <Card.Body>
+     <Card.Title>{this.state.cityData.display_name}</Card.Title>
+     <Card.Img src={this.state.map} />
+     <Card.Text>{`Latitude: ${this.state.cityData.lat}`}</Card.Text>
+     <Card.Text>{`Longitude: ${this.state.cityData.lon}`}</Card.Text>
+   </Card.Body>
+ </Card>
+ </>
        );
 }
 }
